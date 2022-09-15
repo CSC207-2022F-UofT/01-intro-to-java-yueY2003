c207ca96c984921c1353c60962b0946fa3b6c3d3
howiey <howieyu1003@gmail.com>
2022-09-14 22:36:48 -0400

    Code Exercise

--- src/main/java/Basics.java
+++ src/main/java/Basics.java
@@ -8,7 +8,7 @@
  */
 
 /**
- *  1. In Java, no code can exist outside of a class. Unlike Python which uses
+ *  1. In Java, no code can exist outside a class. Unlike Python which uses
  *    functions, all code in Java uses methods.
  *
  *    For this exercise, all of our code is within the Basics class.
@@ -44,7 +44,7 @@ public class Basics {
          *                Hello World!
          */
 
-
+        System.out.println("Hello World!");
 
         /* 4. In Python, we could declare variables using a simple assignment
          *    statement. For example:
@@ -62,7 +62,7 @@ public class Basics {
          * TODO (Task 2): Create a variable named my_variable and assign it the
          *                value 100.
          */
-
+        int my_variable = 100;
 
 
         /* Do not remove the line below: if you did task 2 correctly, then
@@ -99,7 +99,10 @@ public class Basics {
          * Current count: 1
          * Current count: 0
          */
-
+        for (int i = 10; i>= 0; i--)
+        {
+            System.out.println("Current count: " + i);
+        }
 
     }
 
@@ -141,6 +144,17 @@ public class Basics {
          *                you may use them if you wish.
          */
         StringBuilder ret = new StringBuilder();
+        String[] s = to_split.split(" ");
+        ret.append(s[0].charAt(0));
+        ret.append(s[1].charAt(0));
+        ret.append(s[2].charAt(0));
+        ret.append(s[3].charAt(0));
+        ret.append(s[4].charAt(0));
+        ret.append(s[5].charAt(0));
+        ret.append(s[6].charAt(0));
+
+
+
 
         // Fill in the rest of the body here
 
@@ -170,6 +184,13 @@ public class Basics {
          *                You can index into arrays as we do in Python
          *                (e.g. arr[i] gives you the item at index i).
          */
+        for (int i = 0; i < arr.length; i++)
+        {
+            if (i % 2 != 0) {
+
+                current_sum += arr[i];
+            }
+        }
 
         return current_sum;
     }
